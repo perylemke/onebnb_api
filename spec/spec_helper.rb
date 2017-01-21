1b53d3734edf10333969b96cd459020786d0f5d0
Pery Lemke <pery.lemke@gmail.com>
2017-01-21 19:04:47 -0200

    Inclusão de método para limpar as fotos durantes os testes automatizados.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -47,6 +47,12 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  config.after(:each) do
+    if Rails.env.test? || Rails.env.cucumber?
+      FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/user/photo"])
+    end
+  end
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
